5c03532756a360504f0ff6ba3c40483c29a49e4e
최현정 <hjchoi.dev@gmail.com>
2025-11-17 16:21:26 +0900

    Title: 특정 조건 두 정수 비교, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251117/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.print(a < b ? 1 : 0);
+        System.out.print(" ");
+        System.out.print(a == b ? 1 : 0);
+    }
+}
