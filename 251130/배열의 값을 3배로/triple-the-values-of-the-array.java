025db1f9d7d1d87e78cb1b239e4adf362f6b70b9
최현정 <hjchoi.dev@gmail.com>
2025-11-30 23:07:26 +0900

    Title: 배열의 값을 3배로, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

--- 251130/배열의 값을 3배로/triple-the-values-of-the-array.java
+++ 251130/배열의 값을 3배로/triple-the-values-of-the-array.java
@@ -11,7 +11,7 @@ public class Main {
         for (int i = 0; i < 3; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 3; j++) {
-                array[i][j] = (st.nextToken().charAt(0) - '0') * 3;
+                array[i][j] = Integer.parseInt(st.nextToken()) * 3;
                 System.out.print(array[i][j] + " ");
             }
             System.out.println();
