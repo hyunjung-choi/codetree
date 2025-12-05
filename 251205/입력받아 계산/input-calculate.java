df63638995da63b4a48fa92ab80f9c9a5fad25ec
최현정 <hjchoi.dev@gmail.com>
2025-12-05 17:53:34 +0900

    Title: 입력받아 계산, Time: 88ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251205/입력받아 계산/input-calculate.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try {
+            BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+            System.out.println(Integer.parseInt(br.readLine()) + 2);
+        } catch (NumberFormatException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
