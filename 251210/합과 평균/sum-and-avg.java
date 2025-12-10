add407cff6ff1b189fe5308c9d73db1c4e6549b7
최현정 <hjchoi.dev@gmail.com>
2025-12-10 21:44:44 +0900

    Title: 합과 평균, Time: 94ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251210/합과 평균/sum-and-avg.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            int sum = a + b;
+
+            System.out.printf("%d %.1f", sum, sum / 2.0);
+        }
+    }
+}
