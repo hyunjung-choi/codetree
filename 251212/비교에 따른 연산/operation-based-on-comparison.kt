6d0abb14b814aefb7b66fa4eea0337cd4dba4511
최현정 <hjchoi.dev@gmail.com>
2025-12-12 09:52:53 +0900

    Title: 비교에 따른 연산, Time: 127ms, Memory: 12MB, Status: Passed - Codetree

---
+++ 251212/비교에 따른 연산/operation-based-on-comparison.kt
@@ -0,0 +1,10 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val (a, b) = br.readLine().split(" ").map { it.toInt() }
+    
+    if (a > b) println(a * b)
+    else println(b / a)
+}
