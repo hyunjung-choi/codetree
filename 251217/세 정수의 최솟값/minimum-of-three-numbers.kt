37d1bb120cbf91ea3effc3c33e0388b68a54b870
최현정 <hjchoi.dev@gmail.com>
2025-12-17 23:49:23 +0900

    Title: 세 정수의 최솟값, Time: 126ms, Memory: 12MB, Status: Passed - Codetree

--- 251217/세 정수의 최솟값/minimum-of-three-numbers.kt
+++ 251217/세 정수의 최솟값/minimum-of-three-numbers.kt
@@ -2,7 +2,7 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
-    val (a, b, c) = readLine().split(" ").map { it.toInt() }
+    val (a, b, c) = readLine().trim().split(" ").map { it.toInt() }
 
     val minValue = minOf(a, b, c)
     
