6afb7cde66f815f327108fdc2fefa3372eea686e
최현정 <hjchoi.dev@gmail.com>
2026-01-02 20:09:56 +0900

    Title: 1부터 n까지 출력, Time: 146ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 260102/1부터 n까지 출력/print-from-1-to-n.kt
@@ -0,0 +1,7 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val n = readLine().toInt()
+    println((1..n).joinToString(" "))
+}
