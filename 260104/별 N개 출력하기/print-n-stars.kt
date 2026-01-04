3f3600d0dacbd99cdef1a21b043abd2b2c7f9020
최현정 <hjchoi.dev@gmail.com>
2026-01-04 20:38:45 +0900

    Title: 별 N개 출력하기, Time: 121ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 260104/별 N개 출력하기/print-n-stars.kt
@@ -0,0 +1,10 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val n = readLine().trim().toInt()
+    
+    repeat(n) {
+        println("*")
+    }
+}
