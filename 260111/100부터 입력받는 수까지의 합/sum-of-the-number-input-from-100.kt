ae518e508f6abe94cbe4de0c414ac4b59035f50a
최현정 <hjchoi.dev@gmail.com>
2026-01-11 20:24:21 +0900

    Title: 100부터 입력받는 수까지의 합, Time: 154ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 260111/100부터 입력받는 수까지의 합/sum-of-the-number-input-from-100.kt
@@ -0,0 +1,7 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val n = readLine().trim().toInt()
+    println((n..100).sum())
+}
